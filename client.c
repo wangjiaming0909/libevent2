7afe735a1d631e08f50d15278ecdfdb285c3db11
mrw <604227650@qq.com>
2017-06-10 14:11:19 +0800

    server_socket

--- client.c
+++ client.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <event.h>
 
+const char*     g_szServerIP = "139.224.239.147";
+const short     g_nConnectPort = 9090;
 
 int main(){
+    int                     fd;
+    struct sockaddr_in      addr;
     
+    fd = socket(PF_INET, SOCK_STREAM, 0);
+    if(fd < 0){
+        printf("socket error..");
+        return -1;
+    }
+
+    
+
+
 }
